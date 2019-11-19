edb6e18dead276d8ee3b9c01656d1e5d64d470d8
Michael Speed <michael.the.don@gmail.com>
2019-11-19 14:47:22 +0530

    stage 2

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Gitartha', some: 'dfhjdjfjdh', some2: [] });
+
+  res.render('index', { title: 'Gitartha', some: 'dfhjdjfjdh',
+    names: [
+      {fname: 'Gitartha', lname: 'Kashyap'},
+      {fname: 'utkal', lname: 'das'},
+      {fname: 'Nihal', lname: 'Buragohain'},
+      {fname: 'Madhuzya', lname: 'Bezbaruah'}
+    ],
+    show: false
+  });
 });
 
 module.exports = router;
