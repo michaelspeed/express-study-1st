5980bd386654235c9471850d8e302a9434f83f74
Michael Speed <michael.the.don@gmail.com>
2020-01-09 17:05:34 +0530

    stage 4

--- app.js
+++ app.js
@@ -18,6 +18,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// publically accessible folder
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -39,5 +41,7 @@ app.use(function(req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });*/
+
+app.listen(5660)
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -4,15 +4,49 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-  res.render('index', { title: 'Gitartha', some: 'dfhjdjfjdh',
+  const fname = 'khushi'
+  const lname = 'shukla'
+  const age = 18
+  const home = 'dibrugarh'
+  const stris = `${fname} ${lname} of age ${age} lives in ${home}`
+  console.log(stris)
+
+  let array = [1,2,3,4,5]
+  array.forEach(item => {
+    console.log(item)
+  })
+
+ /* res.render('index', { title: 'Gitartha', some: 'dfhjdjfjdh', hw: 'dskdjkhffffhfhfhfhfhfh',
     names: [
-      {fname: 'Gitartha', lname: 'Kashyap'},
-      {fname: 'utkal', lname: 'das'},
-      {fname: 'Nihal', lname: 'Buragohain'},
-      {fname: 'Madhuzya', lname: 'Bezbaruah'}
+      {
+        fname: 'Gitartha',
+        lname: 'Kashyap'
+      },
+      {
+        fname: 'utkal',
+        lname: 'das'
+      },
+      {
+        fname: 'Nihal',
+        lname: 'Buragohain'
+      },
+      {
+        fname: 'Madhuzya',
+        lname: 'Bezbaruah'
+      }
     ],
-    show: false
-  });
+    show: true
+  });*/
+ res.render('index', {
+   fname,
+   lname,
+   age,
+   home
+ })
 });
+
+router.post('/', function (req, res, next) {
+  console.log(req)
+})
 
 module.exports = router;
